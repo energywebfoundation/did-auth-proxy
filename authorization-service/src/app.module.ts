aa0ff9bab25b404e0306fbd48b11ac473e7159b0
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2022-02-01 16:58:11 +0100

    chore: ConfigModule + env vars validation and defaults

--- authorization-service/src/app.module.ts
+++ authorization-service/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
+const validationOptions = {
+  allowUnknown: true,
+  abortEarly: false,
+};
+
+export const validationSchema = Joi.object({
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'test')
+    .default('development'),
+});
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationOptions,
+      validationSchema,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
