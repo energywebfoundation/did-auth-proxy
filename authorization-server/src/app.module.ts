38bbd1c1b67a92e7fa231cb313025ec7a35c238b
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2022-02-03 14:01:00 +0100

    chore: removed unused controller

--- authorization-server/src/app.module.ts
+++ authorization-server/src/app.module.ts
@@ -1,5 +1,4 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
@@ -37,7 +36,7 @@ export const validationSchema = Joi.object({
     }),
     AuthModule,
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
