68937d8275cff729cd420c01920197799093f5e6
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2022-03-07 15:56:50 +0100

    chore: fixed linting errors

--- authorization-server/src/auth/auth.controller.ts
+++ authorization-server/src/auth/auth.controller.ts
@@ -16,7 +16,12 @@ import { LoginGuard } from './login.guard';
 import { decode as decodeJWT } from 'jsonwebtoken';
 import { JwtAuthGuard } from './jwt.guard';
 import { LoginResponseDataDto } from './dto/login-response-data.dto';
-import { ApiBearerAuth, ApiBody, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOkResponse,
+  ApiOperation,
+} from '@nestjs/swagger';
 import { LoginDataDTO } from './dto/login-data.dto';
 import { ConfigService } from '@nestjs/config';
 import { RefreshDto } from './dto/refresh.dto';
@@ -40,10 +45,10 @@ export class AuthController {
 
   @Get()
   @ApiOperation({
-    description: 'Returns 200 response code with "OK"'
+    description: 'Returns 200 response code with "OK"',
   })
   public async getStatus(): Promise<string> {
-      return 'OK';
+    return 'OK';
   }
 
   @Post('login')
