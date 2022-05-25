bc27e63b0546d4a676460a94a3f61c31f7540b98
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2022-05-25 16:46:05 +0200

    refactor: moved guards files to sub folder

--- authorization-server/src/auth/auth.controller.ts
+++ authorization-server/src/auth/auth.controller.ts
@@ -11,9 +11,8 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Request, Response } from 'express';
-import { LoginGuard } from './login.guard';
+import { JwtAuthGuard, LoginGuard, ValidRefreshTokenGuard } from './guards';
 import { decode as decodeJWT } from 'jsonwebtoken';
-import { JwtAuthGuard } from './jwt.guard';
 import { LoginResponseDto } from './dto/login-response.dto';
 import {
   ApiBearerAuth,
@@ -32,7 +31,6 @@ import {
 } from './auth.interface';
 import { LoggerService } from '../logger/logger.service';
 import { LogoutDto } from './dto/logout.dto';
-import { ValidRefreshTokenGuard } from './guards/valid-refresh-token.guard';
 
 @Controller('auth')
 @UsePipes(
