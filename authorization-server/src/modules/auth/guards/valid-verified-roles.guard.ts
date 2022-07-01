37c574439ad9d591e6b43b58ed716e4dc14d80fa
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2022-07-01 16:01:38 +0200

    fix: validate verified roles (#112)

---
+++ authorization-server/src/modules/auth/guards/valid-verified-roles.guard.ts
@@ -0,0 +1,50 @@
+import { CanActivate, ExecutionContext, Inject } from '@nestjs/common';
+import { RolesValidationService } from '../roles-validation.service';
+import { decode as decodeJWT } from 'jsonwebtoken';
+import { IDidAccessTokenPayload } from '../types';
+import { LoggerService } from '../../logger';
+
+//TODO: test like this: https://stackoverflow.com/questions/55848238/nestjs-unit-test-mock-method-guard
+
+export class ValidVerifiedRolesGuard implements CanActivate {
+  constructor(
+    @Inject(LoggerService)
+    private readonly logger: LoggerService,
+    private readonly rolesValidationService: RolesValidationService,
+  ) {
+    logger.setContext(ValidVerifiedRolesGuard.name);
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const { user } = context.switchToHttp().getRequest();
+    if (!user) {
+      return false;
+    }
+
+    const didAccessTokenPayload = decodeJWT(
+      user,
+    ) as unknown as IDidAccessTokenPayload;
+
+    this.logger.debug(
+      `validating verified roles: ${JSON.stringify(
+        didAccessTokenPayload.verifiedRoles,
+      )}`,
+    );
+
+    const verifiedRolesAreValid =
+      await this.rolesValidationService.didAccessTokenRolesAreValid(
+        didAccessTokenPayload.verifiedRoles,
+      );
+
+    if (!verifiedRolesAreValid) {
+      const errorMessage = `unexpected verified roles: ${JSON.stringify(
+        didAccessTokenPayload.verifiedRoles,
+      )}`;
+
+      this.logger.error(errorMessage);
+      throw new Error(errorMessage);
+    }
+
+    return verifiedRolesAreValid;
+  }
+}
