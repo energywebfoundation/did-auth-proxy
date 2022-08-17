69ce6b11a5f66fc44fe9122587708d9fbc94fb5c
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2022-08-17 13:28:55 +0200

    style: getting rid of the Prettier errors

--- authorization-server/src/modules/auth/strategies/auth.strategy.ts
+++ authorization-server/src/modules/auth/strategies/auth.strategy.ts
@@ -12,15 +12,20 @@ export class AuthStrategy extends PassportStrategy(LoginStrategy, 'login') {
   ) {
     const projectId = '2DTpW5Ddx5odgzd8tY1lzPCRSeF'; // 2DT... is from JH infura porject - to be deleted
     const projectSecret = 'f0b2110757d2c0642c5d7a62ed84d9bf'; // f0b.. is from JHInfura project - to be deleted
-    const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
+
+    const auth =
+      'Basic ' +
+      Buffer.from(projectId + ':' + projectSecret).toString('base64');
+
     const ipfsClientConfig = {
-        host: 'ipfs.infura.io',
-        port: 5001,
-        protocol: 'https',
-        headers: {
-          authorization: auth
-        }
-    }
+      host: 'ipfs.infura.io',
+      port: 5001,
+      protocol: 'https',
+      headers: {
+        authorization: auth,
+      },
+    };
+
     super({
       jwtSecret: process.env.JWT_SECRET,
       jwtSignOptions: { algorithm: 'HS256' },
@@ -30,7 +35,7 @@ export class AuthStrategy extends PassportStrategy(LoginStrategy, 'login') {
       privateKey: process.env.CACHE_SERVER_LOGIN_PRVKEY,
       didContractAddress: process.env.DID_REGISTRY_ADDRESS,
       ensRegistryAddress: process.env.ENS_REGISTRY_ADDRESS,
-      ipfsClientConfig: ipfsClientConfig
+      ipfsClientConfig: ipfsClientConfig,
     });
 
     this.logger.setContext(AuthStrategy.name);
