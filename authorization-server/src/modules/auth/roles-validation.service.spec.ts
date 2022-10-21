ba9d584342d7c0fa52a223bd270c919ea8530359
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2022-10-21 19:18:42 +0200

    tests: adding more test cases to cover new use cases

--- authorization-server/src/modules/auth/roles-validation.service.spec.ts
+++ authorization-server/src/modules/auth/roles-validation.service.spec.ts
@@ -43,10 +43,53 @@ describe('RolesValidationService', function () {
           },
         ]),
       ).toBe(true);
+
+      expect(
+        await service.didAccessTokenRolesAreValid([
+          {
+            name: 'role1',
+            namespace: 'role1.roles.app1.apps.org1.iam.ewc',
+            status: RoleCredentialStatus.VALID,
+          },
+          {
+            name: 'role2',
+            namespace: 'role2.roles.app1.apps.org1.iam.ewc',
+            status: RoleCredentialStatus.EXPIRED,
+          },
+        ]),
+      ).toBe(true);
     });
   });
 
   describe('when called with unexpected verifiedRoles', function () {
+    it('should resolve to false', async function () {
+      expect(
+        await service.didAccessTokenRolesAreValid([
+          {
+            name: 'role2',
+            namespace: 'role2.roles.app1.apps.org1.iam.ewc',
+            status: RoleCredentialStatus.VALID,
+          },
+        ]),
+      ).toBe(false);
+    });
+  });
+
+  describe('when called with an expired but acceptable role', function () {
+    it('should resolve to false', async function () {
+      expect(
+        await service.didAccessTokenRolesAreValid([
+          {
+            name: 'role1',
+            namespace: 'role1.roles.app1.apps.org1.iam.ewc',
+            status: RoleCredentialStatus.EXPIRED,
+          },
+        ]),
+      ).toBe(false);
+    });
+  });
+
+  describe('when called with a valid but not acceptable role', function () {
     it('should resolve to false', async function () {
       expect(
         await service.didAccessTokenRolesAreValid([
