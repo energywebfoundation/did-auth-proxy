4fac583cd3d3886d7ab7b185d92d17e9ab328662
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2023-01-16 17:06:47 +0100

    feat: accept empty `ACCEPTED_ROLES`

--- authorization-server/src/modules/app/env-vars-validation-schema.ts
+++ authorization-server/src/modules/app/env-vars-validation-schema.ts
@@ -29,7 +29,7 @@ export const envVarsValidationSchema = Joi.object({
   IPFS_PROJECTID: Joi.string().optional().allow(''),
   IPFS_PROJECTSECRET: Joi.string().optional().allow(''),
 
-  ACCEPTED_ROLES: Joi.string().required(),
+  ACCEPTED_ROLES: Joi.string().optional().allow(''),
 
   REDIS_HOST: Joi.string().hostname().default('127.0.0.1'),
   REDIS_PORT: Joi.number().port().default(6379),
