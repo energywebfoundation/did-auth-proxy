7c8decece462e746277dcc4663dd8357f5af66ac
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2023-02-20 18:56:26 +0100

    feat: changing access token source to depend on `AUTH_COOKIE_ENABLED` and `AUTH_COOKIE_ONLY`

--- authorization-server/src/modules/auth/strategies/jwt.strategy.ts
+++ authorization-server/src/modules/auth/strategies/jwt.strategy.ts
@@ -1,11 +1,32 @@
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ExtractJwt, JwtFromRequestFunction, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import * as express from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(config: ConfigService) {
+    const functions: JwtFromRequestFunction[] = [];
+
+    const fromHeader = ExtractJwt.fromAuthHeaderAsBearerToken();
+
+    const fromCookie = (req: express.Request) =>
+      req &&
+      req.cookies &&
+      req.cookies[config.get<string>('AUTH_COOKIE_NAME_ACCESS_TOKEN')];
+
+    if (config.get<boolean>('AUTH_COOKIE_ENABLED')) {
+      if (!config.get<boolean>('AUTH_COOKIE_ONLY')) {
+        functions.push(fromCookie);
+      } else {
+        functions.push(fromHeader);
+        functions.push(fromCookie);
+      }
+    } else {
+      functions.push(fromHeader);
+    }
+
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
         ExtractJwt.fromAuthHeaderAsBearerToken(),
