244d1b1ff42e26cfd7ec6c6caf6d8afabb97c40d
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2023-02-21 14:50:23 +0100

    fix: fixing token extraction functions not applied correctly

--- authorization-server/src/modules/auth/strategies/jwt.strategy.ts
+++ authorization-server/src/modules/auth/strategies/jwt.strategy.ts
@@ -7,7 +7,7 @@ import * as express from 'express';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(config: ConfigService) {
-    const functions: JwtFromRequestFunction[] = [];
+    const extractorFunctions: JwtFromRequestFunction[] = [];
 
     const fromHeader = ExtractJwt.fromAuthHeaderAsBearerToken();
 
@@ -17,27 +17,18 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
       req.cookies[config.get<string>('AUTH_COOKIE_NAME_ACCESS_TOKEN')];
 
     if (config.get<boolean>('AUTH_COOKIE_ENABLED')) {
-      if (!config.get<boolean>('AUTH_COOKIE_ONLY')) {
-        functions.push(fromCookie);
+      if (config.get<boolean>('AUTH_COOKIE_ONLY')) {
+        extractorFunctions.push(fromCookie);
       } else {
-        functions.push(fromHeader);
-        functions.push(fromCookie);
+        extractorFunctions.push(fromHeader);
+        extractorFunctions.push(fromCookie);
       }
     } else {
-      functions.push(fromHeader);
+      extractorFunctions.push(fromHeader);
     }
 
     super({
-      jwtFromRequest: ExtractJwt.fromExtractors([
-        ExtractJwt.fromAuthHeaderAsBearerToken(),
-        (req) => {
-          return (
-            req &&
-            req.cookies &&
-            req.cookies[config.get<string>('AUTH_COOKIE_NAME_ACCESS_TOKEN')]
-          );
-        },
-      ]),
+      jwtFromRequest: ExtractJwt.fromExtractors(extractorFunctions),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET,
     });
