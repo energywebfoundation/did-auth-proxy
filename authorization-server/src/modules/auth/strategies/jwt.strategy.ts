ce127d4c562ca423a9f0fe4cf054a325a2d355e0
Artur Sudnik-Hrynkiewicz <artur.hrynkiewicz@energyweb.org>
2023-02-23 10:23:30 +0100

    fix: getting secret from the config service not `process.env`

--- authorization-server/src/modules/auth/strategies/jwt.strategy.ts
+++ authorization-server/src/modules/auth/strategies/jwt.strategy.ts
@@ -30,7 +30,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors(extractorFunctions),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey: config.get<string>('JWT_SECRET'),
     });
   }
 
